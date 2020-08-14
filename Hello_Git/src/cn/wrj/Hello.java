84d2aadbd195756ae5541dd0631cb56c4d71bd3d
wrj <1838265433@qq.com>
2020-08-14 16:20:22 +0800

    第三次提交Hello.java

--- Hello_Git/src/cn/wrj/Hello.java
+++ Hello_Git/src/cn/wrj/Hello.java
@@ -3,6 +3,5 @@ package cn.wrj;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello,Git!");
-        System.out.println("hello,Tow,Git!");
     }
 }
